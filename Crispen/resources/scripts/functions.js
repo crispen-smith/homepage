65564342282acb892e7283890bd2f202a40f7af8
Crispen Smith <crispen.smith@example.com>
2013-04-10 01:51:38 -0400

    first draft complete

--- Crispen/resources/scripts/functions.js
+++ Crispen/resources/scripts/functions.js
@@ -17,31 +17,56 @@ $(document).ready(function (){
 
 /*Call Picture fill inline so that results can be acted on. */
 
+var width = $(window).width();
 
-
+var sceneImgs = $(".bg");
 var scenes = $(".scene");
-
-scenes.swipe ( {
+scenes.css("position", "fixed");
+scenes.css("left", function(index) {
+	return String(((index * 200) - 50) + "%");
+});
+var lastScene = -(scenes.length-1);
+var currentScene = firstScene = 0;
+sceneImgs.swipe ( {
 			triggerOnTouchEnd : true,
 			swipeStatus : swipeStatus,
 			allowPageScroll:"vertical"
 });
+var navAs = $("nav a");
+navAs.removeAttr("href");
+navAs.on("click", function(event) {
+	currentIndex = -(jQuery.inArray(this, navAs));
+	changeScene(currentIndex);
+});
 
 function swipeStatus(event, phase, direction, distance, fingers)
 { 
-  
+	if(phase == "end" && (direction == "left" || direction == "right") && distance > 120)
+	{
+		if(direction == "left") {
+			if(currentScene == lastScene) return;
+			currentScene--;
+			changeScene(currentScene);
+		}
+		if(direction == "right") {
+			if(currentScene == firstScene) return;
+			currentScene++;
+			changeScene(currentScene);
+		}
+	}
 }
 
-	function scrollImages(distance, duration)
-		{
-			var support = $.support
-			scenes.css("-webkit-transition-duration", (duration/1000).toFixed(1) + "s");
+function changeScene(currentScene) {
+	$(".selected").removeClass("selected");
+	$(scenes).each(function(index, value) {
+		newLeft =  String((((index+currentScene) * 200) - 50) + "%");
+		$(this).animate({ "left": newLeft  }, 1000);
+	});
+	
+	$($("nav a")[-(currentScene)]).addClass("selected");
+}
 
-			//inverse the number we set in the css
-			var value = (distance<0 ? "" : "-") + Math.abs(distance).toString();
 
-			scenes.css("-webkit-transform", "translate3d("+value +"px,0px,0px)");
-		}
 });
 
 
